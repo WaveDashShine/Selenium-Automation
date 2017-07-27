307fb444d5553e5d17a173668b0f4874cf4d39da
Erik <erikcui9020@gmail.com>
2017-07-26 22:05:37 -0700

    git init and stubs

---
+++ Selenium Web Automation/Selenium Web Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium Web Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium Web Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d85a579e-eb4e-4d84-853f-4d8693c87e42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium Web Automation/Selenium Web Automation/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Selenium_Web_Automation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("https://www.google.ca");
+        }
+    }
+}
